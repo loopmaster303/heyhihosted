8835721ae13e1d6bcfa5cd4dafb30933eeccb247
Johnny Mysterio <maation@gmail.com>
2025-07-19 17:16:40 +0000

    stepo 4

--- src/components/page/AppContent.tsx
+++ src/components/page/AppContent.tsx
@@ -2,16 +2,11 @@
 "use client";
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
 import HomePage from '@/app/page';
 
 export default function AppContent() {
-  const router = useRouter();
-  
   // This component is now effectively deprecated and just renders the home page.
   // The actual routing is handled by Next.js file system router.
   // All other logic has been moved to the respective page components.
   return <HomePage />;
 }
-
-    
