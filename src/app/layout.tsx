c678ddbf90e8521cd4d33582595fc5c2a54dad07
loopmaster303 <meckeljohn@icloud.com>
2025-12-29 12:07:54 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
